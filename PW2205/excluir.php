3615a56c337c4042f5b6048ae78d3086ff147fca
Nii <100323103+Nimilanez@users.noreply.github.com>
2023-05-29 14:06:46 -0300

    Add files via upload

---
+++ PW2205/excluir.php
@@ -0,0 +1,18 @@
+<?php
+define('MYSQL_HOST', 'localhost:3306');
+define('MYSQL_USER', 'root');
+define('MYSQL_PASSWORD', '');
+define('MYSQL_DB_NAME', 'cadastro');
+
+try {
+    $pdo = new PDO('mysql:host=' . MYSQL_HOST . ';dbname=' . MYSQL_DB_NAME, MYSQL_USER, MYSQL_PASSWORD);
+    $id = (int)$_GET['id'];
+    $sqlDeletar = "DELETE FROM dadoscliente WHERE id = $id";
+    $deletar = $pdo->prepare($sqlDeletar);
+    $deletar->execute();
+
+    echo "Excluído com sucesso!";
+} catch (PDOException $ex) {
+    echo "Erro ao tentar fazer a conexão com MYSQL: " . $ex->getMessage();
+}
+?>
